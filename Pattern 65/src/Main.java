a9a7e24083181bdf534142c9cb82a20da330884d
SRIDHARSHAN K M D <87009056+Sridharshan18@users.noreply.github.com>
2023-11-11 15:24:53 +0530

    Add files via upload

---
+++ Pattern 65/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i = 1 ; i<=n ; i++)
+        {
+            for(int j = 1 ; j<=n-i+1; j++)
+            {
+                System.out.print(j+" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+    Output:
+         1 2 3 4 5
+         1 2 3 4
+         1 2 3
+         1 2
+         1
+ */
